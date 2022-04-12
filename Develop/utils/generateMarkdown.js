9b1d7123529a44d3632dbb278e765d891338f3b4
Obtains <97128583+Obtains@users.noreply.github.com>
2022-04-12 16:29:14 -0400

    created array for license badges

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,22 @@
+// Created array for the license badges
+const licenseArray = ["Apache 2.0 License", "Boost Software License 1.0", "BSD 3-Clause License", "BSD 2- Clause License", "Eclipse Public License 1.0", "The MIT License", "The Artistic License", "GNU GPL v3", "GNU GPL v2", "GNU AGPL v3", "GNU LGPL v3", "GNU FDL v1.3", "IBM Public License Version 1.0", "ISC License (ISC)"]
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
