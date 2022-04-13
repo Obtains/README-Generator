c79d45d7a71ac2adf234191eae4b991a8353d59c
Obtains <97128583+Obtains@users.noreply.github.com>
2022-04-13 11:06:02 -0400

    made a function that returns the license section of README

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,6 @@
 // Created array for the license badges
 const licenseArray = ["Apache 2.0 License", "Boost Software License 1.0", "BSD 3-Clause License", "BSD 2- Clause License", "Eclipse Public License 1.0", "The MIT License", "The Artistic License", "GNU GPL v3", "GNU GPL v2", "GNU AGPL v3", "GNU LGPL v3", "GNU FDL v1.3", "IBM Public License Version 1.0", "ISC License (ISC)"]
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === licenseArray[0]) {
@@ -50,7 +50,7 @@ function renderLicenseBadge(license) {
   }
 };
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === licenseArray[0]) {
@@ -102,7 +102,21 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'no license') {
+    return `
+    
+    ## [License](#table-of-contents)
+
+    License badge applies to the following:
+
+    ${renderLicenseLink(license)}
+    `;
+  }
+  else {
+    return "";
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
